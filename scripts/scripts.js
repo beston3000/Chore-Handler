05bcc7ae1f54c19d1f8682aeb38650422725ebfc
beston3000 <106094097+beston3000@users.noreply.github.com>
2024-09-13 18:31:01 -0700

    w

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,3 +5,13 @@ function AddMoney(Name, Amount) {
 function RemoveMoney(Name, Amount) {
     localStorage.setItem(Name, localStorage.getItem(Name) - Amount)
 }
+
+function CreateTable() {
+    const body = document.body,
+          tbl = document.createElement('table');
+    tbl.style.width = '100px';
+    tbl.style.border = '1px solid black';
+
+    
+    body.appendChild(tbl);
+  }
