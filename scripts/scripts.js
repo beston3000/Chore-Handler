7b8caad9c3ef6ee60936d8ff6973711a0eeef665
beston3000 <106094097+beston3000@users.noreply.github.com>
2024-09-13 18:23:03 -0700

    wdadadadadada

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 function AddMoney(Name, Amount) {
-    
+    localStorage.setItem(Name, localStorage.getItem(Name) + Amount)
 }
 
 function RemoveMoney(Name, Amount) {
-
+    localStorage.setItem(Name, localStorage.getItem(Name) - Amount)
 }
